ffed59c5087618dc8982be1b61c92eb2187c1de1
KarlenMisakyan <k112-117@yandex.ru>
2021-03-04 21:32:03 +0300

    adidas boot 3d commit

--- app.js
+++
@@ -1,8 +0,0 @@
-// mo
-const card = document.querySelector('.card');
-const container = document.querySelector('.container');
-
-
-container.addEventListener('mousemove', e => {
-    console.log(e.pageX);   
-})
